ecb24460c65769476dc2f357fca002da4e135604
David Maul <dmaul12@gmail.com>
2016-07-01 15:32:32 -0400

    added url to heroku db

--- service/mtaService.js
+++ service/mtaService.js
@@ -1,7 +1,7 @@
 //es6 uses deconstructors
 const {MongoClient}    = require('mongodb')
 
-const dbConnection   = 'mongodb://localhost:27017/transit'
+const dbConnection   = process.env['MONGODB_URI'] || 'mongodb://localhost:27017/transit'
 
 
 module.exports = {
